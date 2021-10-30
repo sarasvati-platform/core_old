837fc1b80f09c41a612da030e0dc785525fa1b20
Advaita Krishna das <advaita.krishna.das@gmail.com>
2021-10-30 11:42:31 +0000

    stryker

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
-export default {
+module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testMatch: [
-    '**/*.feature.ts'
+    '<rootDir>/tests/**/*.feature.ts'
   ],
   moduleNameMapper: {
     '^@src/(.*)': '<rootDir>/src/$1',
